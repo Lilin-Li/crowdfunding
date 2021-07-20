93203a33c405cf8cc510337d181e105068162c56
lilin <c.p.kevinf13@gmail.com>
2021-07-21 02:01:02 +0800

    整合交易管理器

--- crowd-admin/src/test/java/inc/lilin/crowd/DatabaseTest.java
+++ crowd-admin/src/test/java/inc/lilin/crowd/DatabaseTest.java
@@ -1,10 +1,11 @@
 package inc.lilin.crowd;
 
 import inc.lilin.crowd.admin.AdminApplication;
-import inc.lilin.crowd.admin.domain.TestMapper;
+import inc.lilin.crowd.admin.domain.test.TestMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(classes = AdminApplication.class)
 class DatabaseTest {
@@ -12,8 +13,9 @@ class DatabaseTest {
     @Autowired
     TestMapper dataSource;
 
+    @Transactional
     @Test
     void contextLoads() throws Exception{
-        dataSource.insert(new inc.lilin.crowd.admin.domain.Test("aa"));
+        dataSource.insert(new inc.lilin.crowd.admin.domain.test.Test("aa"));
     }
 }
