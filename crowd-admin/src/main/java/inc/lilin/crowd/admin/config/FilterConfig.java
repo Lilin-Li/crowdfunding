c66db2e740e88cc414950078783f379c4388f6d0
lilin <c.p.kevinf13@gmail.com>
2021-07-22 14:54:58 +0800

    no message

--- crowd-admin/src/main/java/inc/lilin/crowd/admin/config/FilterConfig.java
+++ crowd-admin/src/main/java/inc/lilin/crowd/admin/config/FilterConfig.java
@@ -1,20 +1,16 @@
 package inc.lilin.crowd.admin.config;
 
 import inc.lilin.crowd.admin.Interceptors.CharacterEncodingInterceptors;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class MVC_Config implements WebMvcConfigurer {
+@Configuration
+public class FilterConfig implements WebMvcConfigurer {
 
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
-    }
-
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new CharacterEncodingInterceptors()).addPathPatterns("/**");
 //      HiddenHttpMethodFilter (boot之自動配置已配好)
     }
 }
-
