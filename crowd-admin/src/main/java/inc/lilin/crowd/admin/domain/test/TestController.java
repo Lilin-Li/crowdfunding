4a26c00ac639b64ab887e9f664cb1cc247642454
lilin <c.p.kevinf13@gmail.com>
2021-07-22 08:12:59 +0800

    SpringBoot 熱部署

--- crowd-admin/src/main/java/inc/lilin/crowd/admin/domain/test/TestController.java
+++ crowd-admin/src/main/java/inc/lilin/crowd/admin/domain/test/TestController.java
@@ -8,10 +8,10 @@ import java.util.Map;
 @Controller
 public class TestController {
 
-    @RequestMapping("/test")
+    @RequestMapping("/")
     public String test(Map<String,Object> map){
         map.put("hello","AA");
-        System.out.println(123);
-        return "test";
+        System.out.println(22);
+        return "index";
     }
 }
