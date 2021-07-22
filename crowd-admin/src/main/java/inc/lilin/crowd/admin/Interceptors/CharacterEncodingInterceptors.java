3513949e5a5cb12b06126f45809c4e4ef8ed9d1c
lilin <c.p.kevinf13@gmail.com>
2021-07-22 15:06:13 +0800

    各層加入log.debug訊息

--- crowd-admin/src/main/java/inc/lilin/crowd/admin/Interceptors/CharacterEncodingInterceptors.java
+++ crowd-admin/src/main/java/inc/lilin/crowd/admin/Interceptors/CharacterEncodingInterceptors.java
@@ -1,14 +1,17 @@
 package inc.lilin.crowd.admin.Interceptors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class CharacterEncodingInterceptors implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
+        log.debug("經過攔截器CharacterEncodingInterceptors.preHandle");
         return true;
     }
 }
--- crowd-admin/src/main/java/inc/lilin/crowd/admin/exception_handler/ExceptionHandler.java
+++ crowd-admin/src/main/java/inc/lilin/crowd/admin/exception_handler/ExceptionHandler.java
@@ -1,14 +1,15 @@
 package inc.lilin.crowd.admin.exception_handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
+@Slf4j
 @ControllerAdvice
 public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class})
     public String handleException(Exception ex){
-        System.out.println("例外測試");
+        log.debug("經過例外寵一處理層ExceptionHandler.handleException");
         return "exception";
     }
-
 }
