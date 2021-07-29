21b73972b19b6ff4e63706e47e726f0968105a08
lilin <c.p.kevinf13@gmail.com>
2021-07-30 02:18:44 +0800

    對例外處理層做aop，微改結構

---
+++ crowd-common/src/main/java/inc/lilin/crowd/common/aop/ExceptionHandlerAspect.java
@@ -0,0 +1,37 @@
+package inc.lilin.crowd.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Aspect
+@Component
+public class ExceptionHandlerAspect implements HandlerInterceptor {
+
+    @Pointcut("execution(public * inc.lilin.crowd.*.web.springmvc.exceptionhandler.*.*(..)) && !execution(public * inc.lilin.crowd.common.web.springmvc.exceptionhandler.CommonResolve.*(..))")
+    public void asept() {
+    }
+
+    @Around("asept()")
+    public Object doAroundAdvice(ProceedingJoinPoint joinPoint) {
+
+        Object result = null;
+        try {
+            //=================前置通知=====================
+            log.debug("經過例外統一處理層 " + getClass().getSimpleName() + " 執行 " + Thread.currentThread().getStackTrace()[1].getMethodName());
+            result = joinPoint.proceed();
+            //=================返回通知=====================
+        } catch (Throwable e) {
+            //==================異常通知=====================
+        } finally {
+            //=================後置通知=====================
+        }
+        return result;
+    }
+
+}
--- crowd-common/src/main/java/inc/lilin/crowd/common/web/springmvc/exceptionhandler/OurExceptionHandler.java
+++ crowd-common/src/main/java/inc/lilin/crowd/common/web/springmvc/exceptionhandler/OurExceptionHandler.java
@@ -1,9 +1,6 @@
 package inc.lilin.crowd.common.web.springmvc.exceptionhandler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import inc.lilin.crowd.common.core.system_constant.SystemConstant;
-import inc.lilin.crowd.common.web.springmvc.requestTools.RequestHeadTools;
-import inc.lilin.crowd.common.web.springmvc.responseTools.RestResultDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,30 +16,9 @@ public class OurExceptionHandler {
 
     @ExceptionHandler({Exception.class})
     public ModelAndView handleException(Exception ex, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        log.debug("經過例外統一處理層 " + getClass().getSimpleName() + " 執行 " + Thread.currentThread().getStackTrace()[1].getMethodName());
         ex.printStackTrace();
-        return commonResolve(SystemConstant.EXCEPTION_VIEW, ex, request, response);
+        return CommonResolve.resolve(SystemConstant.EXCEPTION_VIEW, ex, request, response);
     }
 
-    // 對JSON 與 Web請求，分別處理。
-    private ModelAndView commonResolve(String viewName, Exception exception, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        boolean isThis_a_JSON_Request = RequestHeadTools.isThis_a_JSON_Request(request);
 
-        // 如果是Ajax請求
-        if (isThis_a_JSON_Request) {
-            RestResultDTO<Object> resultEntity = RestResultDTO.failed(exception.getMessage());
-
-            String json = new ObjectMapper().writeValueAsString(resultEntity);
-            response.getWriter().write(json);    // 將JSON字串作為響應體返回給瀏覽器
-            // 由於上面已經通過原生的response對像返回了響應，所以不提供ModelAndView對像
-            return null;
-        }
-
-        // 如果不是Ajax請求則建立ModelAndView對像
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", exception);  // 將Exception對像存入模型
-        modelAndView.setViewName(viewName); // 設定對應的檢視名稱
-
-        return modelAndView;
-    }
 }
