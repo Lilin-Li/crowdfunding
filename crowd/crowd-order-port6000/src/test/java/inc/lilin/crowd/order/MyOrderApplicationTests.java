8c1597cc9dcf79f6a1ed701b2b507f45f08c9fba
lilin <c.p.kevinf13@gmail.com>
2021-09-18 16:26:25 +0800

    「建立order模組」

---
+++ crowd/crowd-order-port6000/src/test/java/inc/lilin/crowd/order/MyOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.lilin.crowd.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
